a06027d042b6ce530625d9f639ab738396dd4d0a
prasun-ghosh1998 <prasnbikashghosh1998@gmail.com>
2025-06-23 00:29:41 +0530

    update html and css

--- js/index.js
+++ js/index.js
@@ -1,42 +1,82 @@
 
+
+  AOS.init();
+
+
+// scrolling sec
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
 // navbar
 
-const hamber = document.querySelector(".hamberger");
 const navcontent = document.querySelector(".nav-content");
-const closenav = document.querySelector(".close-bar");
 
-hamber.addEventListener("click", () => {
-  console.log(navcontent.classList);
-  navcontent.classList.toggle("active");
-});
-closenav.addEventListener("click", () => {
-  console.log(navcontent.classList);
-  navcontent.classList.toggle("active");
-});
+function openNav() {
+  if (navcontent.style.width === "50vh") {
+    sidebar.style.width = "0";
+  } else {
+    navcontent.style.width = "54vh";
+  }
+}
+
+function closeNav() {
+  navcontent.style.width = "0";
+}
+
+// counter sec
+
+ const counters = document.querySelectorAll('.counter');
+
+  const speed = 300; // lower is faster
+
+  counters.forEach(counter => {
+    const animate = () => {
+      const target = +counter.getAttribute('data-target');
+      const count = +counter.innerText.replace(/\D/g, ''); // remove non-digits
+      const increment = target / speed;
+
+      if (count < target) {
+        counter.innerText = Math.ceil(count + increment) + (target >= 1000 ? 'k' : '+');
+        setTimeout(animate, 20);
+      } else {
+        counter.innerText = target + (target >= 1000 ? 'k' : '+');
+      }
+    };
+
+    animate();
+  });
 
 
 // team sec
 
-const swiper = new Swiper('.swiper', {
+const swiper1 = new Swiper('#swiper1', {
   loop: true,
   centeredSlides: true,
   spaceBetween: 30,
-
   pagination: {
-    el: '.swiper-pagination',
+    el: '#swiper-pagination1',
   },
-  //   autoplay: {
-  //     delay: 2000,
-  //   },
+    autoplay: {
+      delay: 2300,
+    },
 
   breakpoints: {
     1440:{
       slidesPerView: 3,
-      spaceBetween: 30,
     },
     1024:{
       slidesPerView: 3,
-      spaceBetween: 30,
+    },
+    973:{
+      slidesPerView: 3,
     },
     768:{
       slidesPerView: 2,
@@ -49,26 +89,33 @@ const swiper = new Swiper('.swiper', {
 
   },
 
-
 });
-
 
 
 // client revew
 
 const swiper2 = new Swiper('#swiper2', {
   loop: true,
-  slidesPerView: 1,
+  // slidesPerView: 1,
   centeredSlides: true,
   spaceBetween: 10,
 
   pagination: {
-    el: '.swiper-pagination',
+    el: '#swiper-pagination2',
   },
-  //   autoplay: {
-  //     delay: 2000,
-  //   },
+    autoplay: {
+      delay: 2600,
+    },
+
+});
 
 
+// FAQ Jquery
+
+$(document).ready(function(){
+  $(".flip").click(function(){
+    $(this).next(".panel").slideToggle("slow");
+    $(this).find(".arrow").toggleClass("rotate");
+  });
 });
 
